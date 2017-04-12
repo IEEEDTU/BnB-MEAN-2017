748432ced0d42c984d44cf30f001461c81fe1a79
Nikhil Mishra <nkmishra1997@gmail.com>
2017-04-12 19:36:28 +0530

    fb keys added

--- app/passport/passport.js
+++ app/passport/passport.js
@@ -38,9 +38,9 @@ module.exports = function(app, passport) {
 
     // Facebook Strategy    
     passport.use(new FacebookStrategy({
-            clientID: '310132302703073', // Replace with your Facebook Developer App client ID
-            clientSecret: '2e94e77add384b6e2b2029947c3861b4', // Replace with your Facebook Developer client secret
-            callbackURL: "http://www.herokutestapp3z24.com/auth/facebook/callback", // Replace with your Facebook Developer App callback URL
+            clientID: '210306086123010', // Replace with your Facebook Developer App client ID
+            clientSecret: '7285e8ee9fbcb578ffb83f91e6779047', // Replace with your Facebook Developer client secret
+            callbackURL: "https://bnb-2018.herokuapp.com//auth/facebook/callback", // Replace with your Facebook Developer App callback URL
             profileFields: ['id', 'displayName', 'photos', 'email']
         },
         function(accessToken, refreshToken, profile, done) {
