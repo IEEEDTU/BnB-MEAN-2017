640d97935ef58716c29b7f7d5ea09e79bf29b91b
utkarshvermaa <utkar.sh@outlook.com>
2017-06-03 17:12:29 +0530

    Without Headers | Success

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
 var path = require('path');
-var cron = require('cron');
+// var cron = require('cron');
 var configDB = require('./config/database.js');
 
 mongoose.Promise = global.Promise;
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -11,7 +11,7 @@ export class DataService {
    
     let headers = new Headers({ 'access_token': "EAABoUzKDKp0BAHaGQceOPB5F3zI6WBHMdfKPY2HwICVwFOjIh6icdchkPPrGIBxMJxnXQAYC9VvbJjngZCU1Q2IrMOQ3xdfKXMXIQO4ZC6XO1bGpDbnQWdijBQI4LItRC126GY8ZCblgJhRbSlLex0gtyATamB08aCoMtZBfrgZDZD" });
     let options = new RequestOptions({ headers: headers });
-    return this.http.get('http://localhost:3000/companylist/', options).map(
+    return this.http.get('http://localhost:3000/companylist/').map(
           (res) => res.json()
         )      
   }
